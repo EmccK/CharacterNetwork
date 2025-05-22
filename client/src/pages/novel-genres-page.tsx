7e4c9714f8684fe060d9eaf8bf9b49cb9d3107b2
EmccK <wangkai9799@gmail.com>
2025-05-23 02:14:31 +0800

    feat: 重构小说类型管理页面，移除侧边栏和顶部栏，简化布局

--- client/src/pages/novel-genres-page.tsx
+++ client/src/pages/novel-genres-page.tsx
@@ -1,17 +1,11 @@
-import Sidebar from "@/components/layout/sidebar";
-import Topbar from "@/components/layout/topbar";
 import NovelGenreList from "@/components/novel-genre/novel-genre-list";
 
 export default function NovelGenresPage() {
   return (
-    <div className="flex h-screen bg-gray-50">
-      <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Topbar title="小说类型管理" />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-gray-50">
-          <NovelGenreList />
-        </main>
-      </div>
+    <div className="flex-1 flex flex-col overflow-hidden bg-gray-50">
+      <main className="flex-1 overflow-y-auto bg-gray-50">
+        <NovelGenreList />
+      </main>
     </div>
   );
 }
